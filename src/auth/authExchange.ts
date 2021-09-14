2acf9122d9af10712d95a3a57aeb0510cd2a315a
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-09-14 19:39:35 +0200

    custom authexchange

---
+++ src/auth/authExchange.ts
@@ -0,0 +1,226 @@
+import {
+  pipe,
+  map,
+  mergeMap,
+  fromPromise,
+  fromValue,
+  filter,
+  onStart,
+  empty,
+  take,
+  makeSubject,
+  toPromise,
+  merge,
+  share,
+  takeUntil,
+} from 'wonka';
+
+import {
+  Operation,
+  OperationContext,
+  OperationResult,
+  CombinedError,
+  Exchange,
+  createRequest,
+  makeOperation,
+  TypedDocumentNode,
+} from '@urql/core';
+
+import { DocumentNode } from 'graphql';
+
+export interface AuthConfig<T> {
+  /** addAuthToOperation() must be provided to add the custom `authState` to an Operation's context, so that it may be picked up by the `fetchExchange`. */
+  addAuthToOperation(params: {
+    authState: T | null;
+    operation: Operation;
+  }): Operation;
+
+  /** didAuthError() may be provided to tweak the detection of an authentication error that this exchange should handle. */
+  didAuthError?(params: { error: CombinedError; authState: T | null }): boolean;
+
+  /** willAuthError() may be provided to detect a potential operation that'll receive authentication error so that getAuth() can be run proactively. */
+  willAuthError?(params: {
+    authState: T | null;
+    operation: Operation;
+  }): boolean;
+
+  /** getAuth() handles how the application refreshes or reauthenticates given a stale `authState` and should return a new `authState` or `null`. */
+  getAuth(params: {
+    authState: T | null;
+    /** The mutate() method may be used to send one-off mutations to the GraphQL API for the purpose of authentication. */
+    mutate<Data extends unknown, Variables extends Record<string, unknown>>(
+      query: DocumentNode | TypedDocumentNode<Data, Variables> | string,
+      variables?: Variables,
+      context?: Partial<OperationContext>
+    ): Promise<OperationResult<Data>>;
+    /** The query() method may be used to send one-off queries to the GraphQL API for the purpose of authentication. */
+    query<Data extends unknown, Variables extends Record<string, unknown>>(
+      query: DocumentNode | TypedDocumentNode<Data, Variables> | string,
+      variables?: Variables,
+      context?: Partial<OperationContext>
+    ): Promise<OperationResult<Data>>;
+  }): Promise<T | null>;
+}
+
+const addAuthAttemptToOperation = (
+  operation: Operation,
+  hasAttempted: boolean
+) =>
+  makeOperation(operation.kind, operation, {
+    ...operation.context,
+    authAttempt: hasAttempted,
+  });
+
+export function authExchange<T>({
+  addAuthToOperation,
+  getAuth,
+  didAuthError,
+  willAuthError,
+}: AuthConfig<T>): Exchange {
+  return ({ client, forward }) => {
+    const retryQueue: Map<number, Operation> = new Map();
+    const {
+      source: retrySource$,
+      next: retryOperation,
+    } = makeSubject<Operation>();
+
+    let authState: T | null = null;
+
+    return operations$ => {
+      function mutate<Data extends unknown, Variables extends Record<string, unknown>>(
+        query: DocumentNode | string,
+        variables?: Variables,
+        context?: Partial<OperationContext>
+      ): Promise<OperationResult<Data>> {
+        const operation = client.createRequestOperation(
+          'mutation',
+          createRequest(query, variables),
+          context
+        );
+
+        return pipe(
+          result$,
+          onStart(() => retryOperation(operation)),
+          filter(result => result.operation.key === operation.key),
+          take(1),
+          toPromise
+        );
+      }
+      function query<Data extends unknown, Variables extends Record<string, unknown>>(
+        query: DocumentNode | string,
+        variables?: Variables,
+        context?: Partial<OperationContext>
+      ): Promise<OperationResult<Data>> {
+        const operation = client.createRequestOperation(
+          'query',
+          createRequest(query, variables),
+          context
+        );
+
+        return pipe(
+          result$,
+          onStart(() => retryOperation(operation)),
+          filter(result => result.operation.key === operation.key),
+          take(1),
+          toPromise
+        );
+      }
+
+
+      const updateAuthState = (newAuthState: T | null) => {
+        authState = newAuthState;
+        authPromise = undefined;
+        retryQueue.forEach(retryOperation);
+        retryQueue.clear();
+      };
+
+      let authPromise: Promise<unknown> | void = Promise.resolve()
+        .then(() => getAuth({ authState, mutate, query }))
+        .then(updateAuthState);
+
+      const refreshAuth = (operation: Operation): void => {
+        // add to retry queue to try again later
+        operation = addAuthAttemptToOperation(operation, true);
+        retryQueue.set(operation.key, operation);
+
+        // check that another operation isn't already doing refresh
+        if (!authPromise) {
+          authPromise = getAuth({ authState, mutate, query })
+            .then(updateAuthState)
+            .catch(() => updateAuthState(null));
+        }
+      };
+
+      const sharedOps$ = pipe(operations$, share);
+
+      const teardownOps$ = pipe(
+        sharedOps$,
+        filter((operation: Operation) => {
+          return operation.kind === 'teardown';
+        })
+      );
+
+      const pendingOps$ = pipe(
+        sharedOps$,
+        filter((operation: Operation) => {
+          return operation.kind !== 'teardown';
+        })
+      );
+
+      const opsWithAuth$ = pipe(
+        merge([
+          retrySource$,
+          pipe(
+            pendingOps$,
+            mergeMap(operation => {
+              if (retryQueue.has(operation.key)) {
+                return empty;
+              }
+
+              if (
+                !authPromise &&
+                willAuthError &&
+                willAuthError({ operation, authState })
+              ) {
+                refreshAuth(operation);
+                return empty;
+              } else if (!authPromise) {
+                return fromValue(addAuthAttemptToOperation(operation, false));
+              }
+
+              const teardown$ = pipe(
+                sharedOps$,
+                filter(op => {
+                  return op.kind === 'teardown' && op.key === operation.key;
+                })
+              );
+
+              return pipe(
+                fromPromise(authPromise),
+                map(() => addAuthAttemptToOperation(operation, false)),
+                takeUntil(teardown$)
+              );
+            })
+          ),
+        ]),
+        map(operation => addAuthToOperation({ operation, authState }))
+      );
+
+      const result$ = pipe(merge([opsWithAuth$, teardownOps$]), forward, share);
+
+      return pipe(
+        result$,
+        filter(({ error, operation }) => {
+          if (error && didAuthError && didAuthError({ error, authState })) {
+            if (!operation.context.authAttempt) {
+              refreshAuth(operation);
+              return false;
+            }
+          }
+
+          return true;
+        })
+      );
+    };
+  };
+}
--- src/real.client.tsx
+++ src/real.client.tsx
@@ -9,7 +9,10 @@ import {
   fetchExchange,
   ssrExchange,
 } from "@urql/core";
-import { authExchange } from '@urql/exchange-auth';
+// import { authExchange } from '@urql/exchange-auth';
+// add query to getAuth
+import { authExchange } from './auth/authExchange';
+
 import { Provider } from "urql";
 
 const isServerSide = typeof window === "undefined";
@@ -20,12 +23,108 @@ const ssr = ssrExchange({
   initialState: !isServerSide ? window.__URQL_DATA__ : undefined,
 });
 
+const ME_QUERY = gql`
+  mutation RefreshCredentials($refreshToken: String!) {
+    refreshCredentials(refreshToken: $refreshToken) {
+      refreshToken
+      token
+    }
+  }
+`;
+const REFRESH_TOKEN_MUTATION = gql`
+  mutation RefreshCredentials($refreshToken: String!) {
+    refreshCredentials(refreshToken: $refreshToken) {
+      refreshToken
+      token
+    }
+  }
+`;
+
 const client = createClient({
   url: "http://localhost:3000/graphql",
   exchanges: [
     dedupExchange,
     cacheExchange,
     ssr, // Add `ssr` in front of the `fetchExchange`
+    authExchange({
+      async getAuth({ authState, mutate }) {
+        if (!authState) {
+          const token = getToken();
+          const refreshToken = getRefreshToken();
+
+          if (token && refreshToken) {
+            return { token, refreshToken };
+          }
+
+          return null;
+        }
+
+        const result = await mutate(REFRESH_TOKEN_MUTATION, {
+          refreshToken: authState.refreshToken,
+        });
+
+        if (result.data?.refreshCredentials) {
+          saveAuthData(result.data.refreshCredentials);
+
+          return result.data.refreshCredentials;
+        }
+
+        // This is where auth has gone wrong and we need to clean up and redirect to a login page
+        clearStorage();
+        window.location.reload();
+
+        return null;
+      },
+
+      addAuthToOperation({ authState, operation }) {
+        if (!authState || !authState.token) {
+          return operation;
+        }
+
+        const fetchOptions =
+          typeof operation.context.fetchOptions === 'function'
+            ? operation.context.fetchOptions()
+            : operation.context.fetchOptions || {};
+
+        return makeOperation(operation.kind, operation, {
+          ...operation.context,
+          fetchOptions: {
+            ...fetchOptions,
+            headers: {
+              ...fetchOptions.headers,
+              Authorization: `Bearer ${authState.token}`,
+            },
+          },
+        });
+      },
+
+      didAuthError({ error }) {
+        return error.graphQLErrors.some(
+          e => e.extensions?.code === 'UNAUTHORIZED'
+        );
+      },
+
+      willAuthError({ operation, authState }) {
+        if (!authState) {
+          // Detect our login mutation and let this operation through:
+          return (
+            operation.kind !== 'mutation' ||
+            // Here we find any mutation definition with the "signin" field
+            !operation.query.definitions.some(definition => {
+              return (
+                definition.kind === 'OperationDefinition' &&
+                definition.selectionSet.selections.some(node => {
+                  // The field name is just an example, since register may also be an exception
+                  return node.kind === 'Field' && node.name.value === 'signin';
+                })
+              );
+            })
+          );
+        }
+
+        return false;
+      },
+    }),
     fetchExchange,
   ],
 });
